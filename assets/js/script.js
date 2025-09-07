ce38d14cbe0db66e1af907a8eb21439c1e9f1092
malaak4web <malaak4web@gmail.com>
2025-09-07 22:30:32 +0300

    Added loading indicator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
 jQuery('#generate-summary').click(function(e) {
     e.preventDefault();
     var post_id = jQuery(this).data('post-id');
+    var nonce = jQuery(this).data('nonce');
+    jQuery(this).prop('disabled', true);
+    jQuery(this).text('Generating...');
     jQuery.ajax({
         url: ajaxurl,
         type: 'POST',
-        data: {action: 'generate_summary', post_id: post_id, _wpnonce: jQuery(this).data('nonce')},
+        data: {action: 'generate_summary', post_id: post_id, _wpnonce: nonce},
         success: function(response) {
             if (response.success) {
                 location.reload();
             } else {
                 alert(response.data);
             }
+        },
+        error: function(xhr, status, error) {
+            alert(error);
+        },
+        complete: function() {
+            jQuery(this).prop('disabled', false);
+            jQuery(this).text('Generate Summary');
         }
     });
 });
